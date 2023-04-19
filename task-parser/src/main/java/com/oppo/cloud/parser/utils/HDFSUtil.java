5ba92326313ecf231bb01f6e6c1d3b411093ca73
yuehan-mm <827890027@qq.com>
2023-04-19 13:36:24 +0800

    delete 删除无用代码

--- task-parser/src/main/java/com/oppo/cloud/parser/utils/HDFSUtil.java
+++ task-parser/src/main/java/com/oppo/cloud/parser/utils/HDFSUtil.java
@@ -19,7 +19,6 @@ package com.oppo.cloud.parser.utils;
 import com.oppo.cloud.common.domain.cluster.hadoop.NameNodeConf;
 import com.oppo.cloud.parser.domain.reader.ReaderObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.security.UserGroupInformation;
@@ -28,7 +27,6 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.PrivilegedExceptionAction;
 import java.util.*;
@@ -51,45 +49,6 @@ public class HDFSUtil {
         return null;
     }
 
-//    private static FileSystem getFileSystem(NameNodeConf nameNodeConf) throws Exception {
-//        Configuration conf = new Configuration(false);
-//        conf.setBoolean("fs.hdfs.impl.disable.cache", true);
-//
-//        if (nameNodeConf.getNamenodes().length == 1) {
-//            String defaultFs =
-//                    String.format("hdfs://%s:%s", nameNodeConf.getNamenodesAddr()[0], nameNodeConf.getPort());
-//            conf.set("fs.defaultFS", defaultFs);
-//            URI uri = new URI(defaultFs);
-//            return FileSystem.get(uri, conf);
-//        }
-//
-//        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
-//
-//        String nameservices = nameNodeConf.getNameservices();
-//
-//        conf.set("fs.defaultFS", "hdfs://" + nameservices);
-//        conf.set("dfs.nameservices", nameservices);
-//        conf.set("dfs.client.failover.proxy.provider." + nameservices,
-//                "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider");
-//
-//        for (int i = 0; i < nameNodeConf.getNamenodes().length; i++) {
-//            String r = nameNodeConf.getNamenodes()[i];
-//            conf.set("dfs.namenode.rpc-address." + nameNodeConf.getNameservices() + "." + r,
-//                    nameNodeConf.getNamenodesAddr()[i] + ":" + nameNodeConf.getPort());
-//        }
-//
-//        String nameNodes = String.join(",", nameNodeConf.getNamenodes());
-//        conf.set("dfs.ha.namenodes." + nameNodeConf.getNameservices(), nameNodes);
-//        URI uri = new URI("hdfs://" + nameservices + ":" + nameNodeConf.getPort());
-//        if (StringUtils.isNotBlank(nameNodeConf.getUser())) {
-//            System.setProperty("HADOOP_USER_NAME", nameNodeConf.getUser());
-//        }
-//        if (StringUtils.isNotBlank(nameNodeConf.getPassword())) {
-//            System.setProperty("HADOOP_USER_PASSWORD", nameNodeConf.getPassword());
-//        }
-//
-//        return FileSystem.get(uri, conf);
-//    }
 
     public static String[] readLines(NameNodeConf nameNode, String logPath) throws Exception {
         String filePath = checkLogPath(nameNode, logPath);
